5375600a3b92cde0cb75f94ed056e264b64deeab
Justanothervitor <vitorhugomoreiradelac@gmail.com>
2023-06-14 00:28:56 -0300

    Project created

---
+++ src/test/java/com/prototype/webproject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
