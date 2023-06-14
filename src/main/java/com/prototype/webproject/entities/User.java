38d6321f46514886150c90e00f24d7f7dcf96735
Justanothervitor <vitorhugomoreiradelac@gmail.com>
2023-06-14 02:19:48 -0300

    H2 database, test profile,JPA

--- src/main/java/com/prototype/webproject/entities/User.java
+++ src/main/java/com/prototype/webproject/entities/User.java
@@ -1,12 +1,21 @@
 package com.prototype.webproject.entities;
 
-import java.util.Objects;
 import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User implements Serializable{
+private static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
 	private String phone;
