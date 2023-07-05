bf8899b8e7fd74d7b8a600da42e616eff254062a
Justanothervitor <vitorhugomoreiradelac@gmail.com>
2023-07-05 00:53:03 -0300

    Relation Many-to-Many between Product and OrderItem

--- src/main/java/com/prototype/webproject/entities/ProductProperties.java
+++ src/main/java/com/prototype/webproject/entities/ProductProperties.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -32,6 +35,9 @@ public class ProductProperties implements Serializable{
 	@ManyToMany
 	@JoinTable(name = "Product_Category_Table",joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<CategoryProperties> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy ="id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public ProductProperties()
 	{
@@ -93,6 +99,15 @@ public class ProductProperties implements Serializable{
 
 	public Set<CategoryProperties> getCategories() {
 		return categories;
+	}
+	@JsonIgnore
+	public Set<ClientOrder> getOrders()
+	{
+		Set<ClientOrder> set = new HashSet<>();
+		for(OrderItem x:items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
