df04781c95317b13094fa530cc850fccd7e798e8
Justanothervitor <vitorhugomoreiradelac@gmail.com>
2023-07-11 22:24:31 -0300

    Added Exception treatment to methods FindbyID,Delete and update,because they don't need to return error 500

--- src/main/java/com/prototype/webproject/services/servicesForUsers.java
+++ src/main/java/com/prototype/webproject/services/servicesForUsers.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.prototype.webproject.entities.UserProperties;
 import com.prototype.webproject.repositories.UserRepository;
+import com.prototype.webproject.services.exceptions.DatabaseException;
+import com.prototype.webproject.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class servicesForUsers {
@@ -23,7 +29,7 @@ public class servicesForUsers {
 	public UserProperties findById(Long id)
 	{
 		Optional<UserProperties> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public UserProperties insert(UserProperties obj)
@@ -33,21 +39,33 @@ public class servicesForUsers {
 	
 	public void delete(Long id)
 	{
-		repository.deleteById(id);
+		try {
+		
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public UserProperties update(Long id, UserProperties obj)
 	{
+		try {
 		UserProperties entity = repository.getOne(id);
 		updateData(entity,obj);
 		return repository.save(entity);
-	}
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		}
 
 	private void updateData(UserProperties entity, UserProperties obj) 
 	{
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-	}
+		}
 
 }
